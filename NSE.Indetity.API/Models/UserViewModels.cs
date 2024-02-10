73e8fc5c276ce3b5595f4fecac2488fec79d6ce2
Joaoof <joaodeus400@gmail.com>
2024-02-10 10:59:57 -0300

    feat: Create Entities from my database

---
+++ NSE.Indetity.API/Models/UserViewModels.cs
@@ -0,0 +1,19 @@
+﻿namespace NSE.Indetity.API.Models
+{
+    public class UserRegister
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string PasswordConfirmation { get; set; }
+    }
+
+
+    public class UserLogin
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}

7bfc641b89f9782616e325ef630d16617661b060
Joaoof <joaodeus400@gmail.com>
2024-02-10 11:10:13 -0300

    feat: Created Controller Register and Login

---
+++ NSE.Indetity.API/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.Indetity.API.Models;
+
+namespace NSE.Indetity.API.Controllers
+{
+    public class AuthController : Controller
+    {
+        public async Task<ActionResult> Register(UserRegister userRegister)
+        {
+
+        }
+
+        public async Task<ActionResult> Login(UserLogin userLogin)
+        {
+
+        }
+    }
+}

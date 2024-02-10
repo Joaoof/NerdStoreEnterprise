10d04a54494be02d9c101b59783ede2b0ae0d1fe
Joaoof <joaodeus400@gmail.com>
2024-02-10 11:05:41 -0300

    feat(Model): Apply Data Annotations

--- NSE.Indetity.API/Models/UserViewModels.cs
+++ NSE.Indetity.API/Models/UserViewModels.cs
@@ -1,19 +1,30 @@
-﻿namespace NSE.Indetity.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NSE.Indetity.API.Models
 {
     public class UserRegister
     {
+        [Required(ErrorMessage = "the field {0} is mandatory")]
+        [EmailAddress(ErrorMessage = "the field {0} is in an invalid format")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "the field {0} is mandatory")]
+        [StringLength(100, ErrorMessage = "the field must be between {2} and {1} characters", MinimumLength = 6)]
         public string Password { get; set; }
 
+        [Compare("Password", ErrorMessage = "passwords don't match")]
         public string PasswordConfirmation { get; set; }
     }
 
 
     public class UserLogin
     {
+        [Required(ErrorMessage = "the field {0} is mandatory")]
+        [EmailAddress(ErrorMessage = "the field {0} is in an invalid format")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "the field {0} is mandatory")]
+        [StringLength(100, ErrorMessage = "the field must be between {2} and {1} characters", MinimumLength = 6)]
         public string Password { get; set; }
     }
 }
